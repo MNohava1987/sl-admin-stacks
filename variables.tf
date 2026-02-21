ab6dfa98067059a540e547545bfb416afa20d023
MNohava1987 <mnohava1987@gmail.com>
2026-02-20 19:38:35 -0600

    Fix: Replace invalid semicolons with newlines in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
-variable "spacelift_api_key_id" { type = string; sensitive = true }
-variable "spacelift_api_key_secret" { type = string; sensitive = true }
+variable "spacelift_api_key_id" { type = string
+  sensitive = true }
+variable "spacelift_api_key_secret" { type = string
+  sensitive = true }
 variable "admin_space_id" { type = string }
 
 # GitHub VCS integration id
