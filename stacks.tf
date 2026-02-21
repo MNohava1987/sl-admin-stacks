904a75884d6582f60c6cc38db90e96a77e39dcca
MNohava1987 <mnohava1987@gmail.com>
2026-02-20 21:04:20 -0600

    Feature: Enable administrative and local preview for all child stacks

--- stacks.tf
+++ stacks.tf
@@ -1,28 +1,28 @@
 # Child stacks created in Admin space
 
 resource "spacelift_stack" "platformspaces" {
-  name               = "admin-platformspaces"
-  space_id            = var.admin_space_id
-  repository          = var.repo_platformspaces
-  branch              = var.branch_main
-  # Removed to use default
-  # github_enterprise_id = var.vcs_integration_id
+  name                 = "admin-platformspaces"
+  space_id             = var.admin_space_id
+  repository           = var.repo_platformspaces
+  branch               = var.branch_main
+  administrative       = true
+  enable_local_preview = true
 }
 
 resource "spacelift_stack" "modulespaces" {
-  name               = "admin-modulespaces"
-  space_id            = var.admin_space_id
-  repository          = var.repo_modulespaces
-  branch              = var.branch_main
-  # Removed to use default
-  # github_enterprise_id = var.vcs_integration_id
+  name                 = "admin-modulespaces"
+  space_id             = var.admin_space_id
+  repository           = var.repo_modulespaces
+  branch               = var.branch_main
+  administrative       = true
+  enable_local_preview = true
 }
 
 resource "spacelift_stack" "policies" {
-  name               = "admin-policies"
-  space_id            = var.admin_space_id
-  repository          = var.repo_policies
-  branch              = var.branch_main
-  # Removed to use default
-  # github_enterprise_id = var.vcs_integration_id
+  name                 = "admin-policies"
+  space_id             = var.admin_space_id
+  repository           = var.repo_policies
+  branch               = var.branch_main
+  administrative       = true
+  enable_local_preview = true
 }
