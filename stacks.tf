e96e745f47ef856f8236a5328b1f274353aff305
MNohava1987 <mnohava1987@gmail.com>
2026-02-20 20:00:00 -0600

    Fix: Wrap vcs_integration_id in github_options block

--- stacks.tf
+++ stacks.tf
@@ -5,7 +5,9 @@ resource "spacelift_stack" "platformspaces" {
   space_id            = var.admin_space_id
   repository          = var.repo_platformspaces
   branch              = var.branch_main
-  vcs_integration_id  = var.vcs_integration_id
+  github_options {
+    id = var.vcs_integration_id
+  }
   terraform_workflow_tool = "TERRAFORM"
 }
 
@@ -14,7 +16,9 @@ resource "spacelift_stack" "modulespaces" {
   space_id            = var.admin_space_id
   repository          = var.repo_modulespaces
   branch              = var.branch_main
-  vcs_integration_id  = var.vcs_integration_id
+  github_options {
+    id = var.vcs_integration_id
+  }
   terraform_workflow_tool = "TERRAFORM"
 }
 
@@ -23,6 +27,8 @@ resource "spacelift_stack" "policies" {
   space_id            = var.admin_space_id
   repository          = var.repo_policies
   branch              = var.branch_main
-  vcs_integration_id  = var.vcs_integration_id
+  github_options {
+    id = var.vcs_integration_id
+  }
   terraform_workflow_tool = "TERRAFORM"
 }
